8d29b0ef55ca468ff18a39f2559cb9fccd257d13
John Verz <113103492+johnverz22@users.noreply.github.com>
2024-12-04 12:55:50 +0800

    Revert "added npm"

--- app/views/layout/main.php
+++ app/views/layout/main.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $title; ?></title>
-    <link rel="stylesheet" href="<?= BASE_URL.'/public/css/tailwind.css' ?>">
+    <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <?php
         //import csss files here
